7e861e624467b8da4ee2cbf335c3645f6fa98ef4
RTSDA <av@rockvilletollandsda.org>
2024-11-28 20:18:51 -0500

    Initial Commit

---
+++ SDA Hymnal/SDA_HymnalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SDA_HymnalApp.swift
+//  SDA Hymnal
+//
+//  Created by Benjamin Slingo on 11/28/24.
+//
+
+import SwiftUI
+
+@main
+struct SDA_HymnalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SDA HymnalTests/SDA_HymnalTests.swift
@@ -0,0 +1,17 @@
+//
+//  SDA_HymnalTests.swift
+//  SDA HymnalTests
+//
+//  Created by Benjamin Slingo on 11/28/24.
+//
+
+import Testing
+@testable import SDA_Hymnal
+
+struct SDA_HymnalTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
